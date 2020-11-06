14827a3e9d34ce9eb2fd1fcb5f871aeb7597c92f
baolong.mei <baolong.mei@accenture.com>
2020-11-06 10:06:02 +0800

    20201106-create jd_collectProduceScore.js

--- QuantumultX/DailyBonus/jd_collect/jd_collectProduceScore.js
+++ QuantumultX/DailyBonus/jd_collect/jd_collectProduceScore.js
@@ -90,7 +90,7 @@ function getHomeData() {
           console.log(`${$.name} API请求失败，请检查网路重试`)
         } else {
           data = JSON.parse(data);
-          if (data && data.data['bizCode'] === 0) {
+          if (data && data.data.bizCode === 0) {
             $.secretp = data.data.result.homeMainInfo.secretp;
           }
         }
@@ -116,7 +116,7 @@ function taskPostUrl(functionId, body = {}) {
     body: `functionId=${functionId}&body=${escape(JSON.stringify(body))}&client=wh5&clientVersion=1.0.0`,
     headers: {
       'User-Agent': 'jdapp;android;8.4.2;8.0.0;;network/wifi;model/Mi Note 2;osVer/26;appBuild/71043;psn/|7;psq/1;uid/;adk/;ads/;pap/JA2015_311210|8.4.2|ANDROID 8.0.0;osv/8.0.0;pv/2.23;jdv/;ref/com.jingdong.app.mall.WebActivity;partner/huawei;apprpd/Home_Main;Mozilla/5.0 (Linux; Android 8.0.0; Mi Note 2 Build/OPR1.170623.032; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.99 Mobile Safari/537.36',
-      'Content-Type': 'application/json;charset=utf-8',
+      'Content-Type': 'application/x-www-form-urlencoded',
       'Host': 'api.m.jd.com',
       'Cookie': cookie,
       'Origin': 'https://wbbny.m.jd.com',
