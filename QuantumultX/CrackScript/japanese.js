4c615e2a4d1243b3eab0f734b6db6b0ed294ee43
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2020-12-14 16:13:45 +0800

    Update japanese.js

--- QuantumultX/CrackScript/japanese.js
+++ QuantumultX/CrackScript/japanese.js
@@ -1,17 +1,31 @@
-function re() {
-    var body = $response.body;
-    if (arguments[0].includes("@")) {
-        var regs = arguments[0].split("@");
-        var strs = arguments[1].split("@");
-        for (i = 0; i < regs.length; i++) {
-            var reg = new RegExp(regs[i], "g");
-            body = body.replace(reg, strs[i]);
-        }
-    } else {
-        var reg = new RegExp(arguments[0], "g");
-        body = body.replace(reg, arguments[1]);
-    }
-    $done(body);
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+if (body.data[0].book_id == 10001) {
+	obj = {
+		"data": [{
+			"book_title": "标准日本语初级",
+			"activation_date": "2020-05-01",
+			"activation_status": 0,
+			"activation_code": "526728",
+			"book_id": 10001
+		}, {
+			"book_title": "标准日本语中级",
+			"activation_date": "2020-05-01",
+			"activation_status": 0,
+			"activation_code": "62899",
+			"book_id": 10002
+		}, {
+			"book_title": "标准日本语高级",
+			"activation_date": "2020-05-01",
+			"activation_status": 0,
+			"activation_code": "893000",
+			"book_id": 10003
+		}],
+		"status": 0,
+		"message": "获取图书列表成功"
+	};
 }
 
-re('"data": [^]+\\]', '"data":[{"book_title":"标准日本语初级","activation_date":"2020-05-01","activation_status":0,"activation_code":"526728","book_id":10001},{"book_title":"标准日本语中级","activation_date":"2020-05-01","activation_status":0,"activation_code":"62899","book_id":10002},{"book_title":"标准日本语高级","activation_date":"2020-05-01","activation_status":0,"activation_code":"893000","book_id":10003}]')
+$done({body: JSON.stringify(obj)});
