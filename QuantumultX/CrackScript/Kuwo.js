276bbf77e38cc0d89f6a0d387ca9eb060c0cf9cf
baolong.mei <baolong.mei@accenture.com>
2020-12-21 16:08:03 +0800

    20201221 daily update

--- QuantumultX/CrackScript/Kuwo.js
+++ QuantumultX/CrackScript/Kuwo.js
@@ -12,25 +12,28 @@ KuWo music unlock Svip
 hostname = *.kuwo.cn,
 
 */
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+const vip = '/vip/v2/user/vip';
+const time = '/vip/spi/mservice';
 
-var obj = JSON.parse($response.body);
+if (url.indexOf(vip) != -1) {
+	obj.data["isNewUser"] = "2";
+	obj.data["vipLuxuryExpire"] = "1835312949000";
+	obj.data["time"] = "1961170340993";
+	obj.data["isYearUser"] = "2";
+	obj.data["vipmExpire"] = "1835312949000";
+	obj.data["vipOverSeasExpire"] = "1835312949000";
+	obj.data["vipExpire"] = "1835312949000";
+	obj.data["vip3Expire"] = "1835312949000";
+	body = JSON.stringify(obj);
+}
 
-const path1 = "/vip/v2/user/vip";
-const path2 = "/vip/spi/mservice";
+if (url.indexOf(time) != -1) {
+	obj["isVIPMAutoPay"] = 2;
+	obj["isVIPLuxAutoPay"] = 2;
+	body = JSON.stringify(obj);
+}
 
-if ($request.url.indexOf(path1) != -1) {
-	obj.data.isNewUser = "2";
-	obj.data.isYearUser = "2";
-	obj.data.vipLuxuryExpire = "4102500561000";
-	obj.data.time = "4102500561000";
-	obj.data.vipExpire = "4102500561000";
-	obj.data.vip3Expire = "4102500561000";
-	obj.data.vipmExpire = "4102500561000";
-	obj.data.vipOverSeasExpire = "4102500561000";
-};
-if ($request.url.indexOf(path2) != -1) {
-	obj.isVIPMAutoPay = 2;
-	obj.isVIPLuxAutoPay = 2;
-};
-
-$done({body: JSON.stringify(obj)});
+$done({body});
