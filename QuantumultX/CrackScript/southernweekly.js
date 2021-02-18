23e8ec3f84846ed86e9586f23da785e9870c5ad2
baolong.mei <baolong.mei@accenture.com>
2021-02-18 16:31:54 +0800

    20210218 daily update

--- QuantumultX/CrackScript/southernweekly.js
+++ QuantumultX/CrackScript/southernweekly.js
@@ -15,7 +15,7 @@ function re() {
 }
 
 re('"member_type":\\d', '"member_type":5')
-re('"expire_time":\\w+', '"expire_time":2030-05-05')
+re('"expire_time":\\w+', '"expire_time":"2030-05-05"')
 re('"user":\\{[^\\}]+\\}', '"user":{\"islogin\":true,\"isview\":true,\"isNewsStand\":2,\"member_type\":5,\"expire_time\":\"2030-05-05\",\"isdigg\":true,\"isfav\":true}')
 re('"is_free":\\d', '"is_free":1')
-re('"isbuy":\\w+', '"isbuy":true')
+re('"isbuy":\\w+', '"isbuy":"true"')
