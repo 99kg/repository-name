169bcd0d60c3074674b0b18559fff5da2ea8c82d
baolong.mei <baolong.mei@accenture.com>
2021-03-09 11:44:01 +0800

    20210309 daily update

--- QuantumultX/DailyBonus/01_temp/jd_global.js
+++ QuantumultX/DailyBonus/01_temp/jd_global.js
@@ -82,7 +82,7 @@ $.invites = [];
       $.canHelp = true;
       for (let code of $.invites) {
         if (!code) continue
-        await helpFriend(code)
+        //await helpFriend(code)
         if(!$.canHelp) break
         await $.wait(1000)
       }
@@ -351,11 +351,11 @@ function shareCodesFormat() {
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
-    const readShareCodeRes = await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
-    }
-    console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
+    //  const readShareCodeRes = await readShareCode();
+    //  if (readShareCodeRes && readShareCodeRes.code === 200) {
+    //   $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
+    //  }
+    //  console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
 }
