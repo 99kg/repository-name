0aa571b6145f21eea339e9fa86b55cff7ea387d8
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2021-06-25 19:41:44 +0800

    Update veffecto.js

--- QuantumultX/CrackScript/veffecto.js
+++ QuantumultX/CrackScript/veffecto.js
@@ -6,13 +6,13 @@ obj = {
 			{
 				"autoRenewProductId" : "yearly_premium",
 				"orderId" : "600000729643467",
-				"isTrialPeriod" : false,
+				"isTrialPeriod" : true,
 				"endTime" : 4086063790000,
 				"productId" : "yearly_premium",
 				"productType" : 3,
 				"startTime" : 1624612621000,
 				"orderStatus" : 1,
-				"autoRenewStatus" : true,
+				"autoRenewStatus" : false,
 				"originalOrderId" : "600000729643467",
 				"sign" : "3edbc2ac1a76bf188a97da9123450aba"
 			}
