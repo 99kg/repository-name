744d82c4a15dcc619e60750d7339fb02265f505f
killer <killer@accenture.com>
2021-06-25 18:02:21 +0800

    20210625 daily update

---
+++ QuantumultX/CrackScript/veffecto.js
@@ -0,0 +1,32 @@
+var obj = JSON.parse($response.body);
+obj = {
+	"code" : 200,
+	"data" : {
+		"list" : [
+			{
+				"autoRenewProductId" : "yearly_premium",
+				"orderId" : "600000729643467",
+				"isTrialPeriod" : false,
+				"endTime" : 4086063790000,
+				"productId" : "yearly_premium",
+				"productType" : 3,
+				"startTime" : 1624612621000,
+				"orderStatus" : 1,
+				"autoRenewStatus" : true,
+				"originalOrderId" : "600000729643467",
+				"sign" : "3edbc2ac1a76bf188a97da9123450aba"
+			}
+		],
+		"hasFreedTrialProds" : [
+			"yearly_premium"
+		],
+		"systemDate" : 1624613122839,
+		"hasPurchasedProds" : [
+			"yearly_premium"
+		]
+	},
+	"message" : "successful",
+	"success" : true
+};
+
+$done({body: JSON.stringify(obj)});
