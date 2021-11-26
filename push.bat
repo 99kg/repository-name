b2a8221496a55a2675457928963c2593ac841345
baolong.mei <baolong.mei@accenture.com>
2021-11-26 14:18:04 +0800

    20211126 daily update

--- push.bat
+++ push.bat
@@ -4,6 +4,8 @@ git add .
 
 git commit -m "%date% daily update"
 
+git remote set-url origin https://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@github.com/99kg/repository-name.git
+
 git push -u origin master
 
 pause
