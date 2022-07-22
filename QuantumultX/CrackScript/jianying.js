5a3bf5137356a815868f597790a6745470ac528b
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-22 18:11:34 +0800

    Update jianying.js

--- QuantumultX/CrackScript/jianying.js
+++ QuantumultX/CrackScript/jianying.js
@@ -1,14 +1,7 @@
 let body = JSON.parse($response.body);
 
 
-var resp = JSON.parse(body.response);
-resp.start_time = 1584674770;
-resp.end_time = 1974101063;
-resp.is_cancel_subscribe = false;
-resp.subscribe_type = "auto";
-resp.flag = true;
-JSON.stringify(resp);
-body.response = resp;
+body.response = "{\"flag\":true,\"start_time\":1584674770,\"end_time\":1974101063,\"is_first_subscribe\":true,\"is_cancel_subscribe\":false,\"uid\":\"2005146104519054\",\"subscribe_uid\":\"2005146104519054\",\"subscribe_type\":\"auto\",\"authkey\":\"41137e7b949cc0534ac0d1dffc4814b0\",\"product_id\":\"\",\"subscribe_cycle\":0,\"cycle_unit\":\"\",\"space_cap\":0}"
 
 
 body.data.start_time = 1584674770;
