243cd9891ec778f6b707921ebe3de090480b1c1b
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-22 18:02:03 +0800

    Update jianying.js

--- QuantumultX/CrackScript/jianying.js
+++ QuantumultX/CrackScript/jianying.js
@@ -1,15 +1,15 @@
 let body = JSON.parse($response.body);
 
-if(body.response){
-  var resp = JSON.parse(body.response);
-  resp.start_time = 1584674770;
-  resp.end_time = 1974101063;
-  resp.is_cancel_subscribe = false;
-  resp.subscribe_type = "auto";
-  resp.flag = true;
-  JSON.stringify(resp);
-  body.response = resp;
-}
+
+var resp = JSON.parse(body.response);
+resp.start_time = 1584674770;
+resp.end_time = 1974101063;
+resp.is_cancel_subscribe = false;
+resp.subscribe_type = "auto";
+resp.flag = true;
+JSON.stringify(resp);
+body.response = resp;
+
 
 body.data.start_time = 1584674770;
 body.data.end_time = 1974101063;
