5cd16e455eb82a7496d3386957fffce73347a7ae
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-28 14:01:00 +0800

    Update duit.js

--- QuantumultX/CrackScript/duit.js
+++ QuantumultX/CrackScript/duit.js
@@ -1,20 +1,12 @@
-function re() {
-    var body = $response.body;
-    if (arguments[0].includes("@")) {
-        var regs = arguments[0].split("@");
-        var strs = arguments[1].split("@");
-        for (i = 0; i < regs.length; i++) {
-            var reg = new RegExp(regs[i], "g");
-            body = body.replace(reg, strs[i]);
-        }
-    } else {
-        var reg = new RegExp(arguments[0], "g");
-        body = body.replace(reg, arguments[1]);
-    }
-    $done(body);
-}
+let body = JSON.parse($response.body);
 
-re('"vip_end_at":\\d@"vip_mechanism":\\d', '"vip_end_at":1871655312087@"vip_mechanism":0')
-re('"vip_level":\\d@"latest_vip_level":\\d', '"vip_level":11@"latest_vip_level":11')
-re('"vip_remain":\\d@"svip_mechanism":\\d', '"vip_remain":0@"svip_mechanism":1')
-re('"vip_end_at_mills":\\d@"vip":\\w+', '"vip_end_at_mills":1871655312087@"vip":true')
+body.data.vip_end_at = 1871655312087;
+body.data.vip_mechanism = 0;
+body.data.vip_level = 11;
+body.data.latest_vip_level = 11;
+body.data.vip_remain = 0;
+body.data.svip_mechanism = 1;
+body.data.vip_end_at_mills = 1871655312087;
+body.data.vip = true;
+
+$done({body: JSON.stringify(body)});
