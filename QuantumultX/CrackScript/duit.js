9e7a8e56feb46092d4f6877a5291829789746543
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-28 14:03:29 +0800

    Update duit.js

--- QuantumultX/CrackScript/duit.js
+++ QuantumultX/CrackScript/duit.js
@@ -1,12 +1,12 @@
-let body = JSON.parse($response.body);
+let obj=JSON.parse($response.body);
 
-body.data.vip_end_at = 1871655312087;
-body.data.vip_mechanism = 0;
-body.data.vip_level = 11;
-body.data.latest_vip_level = 11;
-body.data.vip_remain = 0;
-body.data.svip_mechanism = 1;
-body.data.vip_end_at_mills = 1871655312087;
-body.data.vip = true;
+obj.data.vip_end_at=1871655312087;
+obj.data.vip_mechanism=0;
+obj.data.vip_level=11;
+obj.data.latest_vip_level=11;
+obj.data.vip_remain=0;
+obj.data.svip_mechanism=1;
+obj.data.vip_end_at_mills=1871655312087;
+obj.data.vip=true;
 
-$done({body: JSON.stringify(body)});
+$done({body: JSON.stringify(obj)});
