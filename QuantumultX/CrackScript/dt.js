2bd09932ac7ea9168a60c91ee33e84c6773d95d6
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-28 11:23:26 +0800

    Update dt.js

--- QuantumultX/CrackScript/dt.js
+++ QuantumultX/CrackScript/dt.js
@@ -9,5 +9,16 @@ hostname = *.duitang.com
 */
 
 ***********************************/
-body = $response.body.replace(/vip_end_at":\d+/g, 'vip_end_at":1871655312087').replace(/vip_level":\d+/g, 'vip_level":11').replace(/latest_vip_level":\d+/g, 'latest_vip_level":11').replace(/svip_mechanism":\d+/g, 'svip_mechanism":1'). replace(/vip_end_at_mills":\d+/g, 'vip_end_at_mills":1871655312087').replace(/vip":\w+/g, 'vip":true');
-$done({body});
+
+let body = JSON.parse($response.body);
+
+body.data.vip_end_at = 1871655312087;
+body.data.vip_mechanism = 0;
+body.data.vip_level = 11;
+body.data.latest_vip_level = 11;
+body.data.vip_remain = 0;
+body.data.svip_mechanism = 1;
+body.data.vip_end_at_mills = 1871655312087;
+body.data.vip = true;
+
+$done({body: JSON.stringify(body)});
