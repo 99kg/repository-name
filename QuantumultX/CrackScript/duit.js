e2e7b8d57c100becbdf37df88c5172098de7e9ed
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-07-28 13:41:38 +0800

    Update and rename dt.js to duit.js

--- QuantumultX/CrackScript/duit.js
+++ QuantumultX/CrackScript/duit.js
@@ -1,15 +1,3 @@
-/***********************************
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️
-/*
-[mitm]
-hostname = *.duitang.com
-[rewrite_local]
-^https:\/\/www\.duitang\.com\/napi\/people\/me\/ url script-response-body https://raw.githubusercontent.com/Geekey101/-X-Public/main/-X-Public-Scripts/duitang.js
-*/
-
-***********************************/
-
 function re() {
     var body = $response.body;
     if (arguments[0].includes("@")) {
