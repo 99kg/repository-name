ce155a4644be3e0b3f562574b6693b01bd87a963
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2022-08-26 14:24:11 +0800

    Update qyxj.js

--- QuantumultX/CrackScript/qyxj.js
+++ QuantumultX/CrackScript/qyxj.js
@@ -6,7 +6,6 @@ https://commerce-api.faceu.mobi/commerce/v1/subscription/user_info url script-re
 [mitm] 
 hostname=commerce-api.faceu.mobi
 ***********************************/
-
 let body = JSON.parse($response.body);
 
 body.data.start_time = 1661493402;
@@ -14,7 +13,10 @@ body.data.end_time = 4102415999;
 body.data.is_cancel_subscribe = false;
 body.data.flag = true;
 body.data.subscribe_type = "auto";
-body.data.cycle_unit = "YEAR";
+body.data.cycle_unit = "MONTH";
+body.data.product_id = "beauty_china_subscription_1month";
+body.data.subscribe_cycle = 1;
+body.data.space_cap = 0;
 
 if(body.response){
      var tmp = JSON.parse(body.response);
@@ -23,8 +25,11 @@ if(body.response){
      tmp.is_cancel_subscribe = false;
      tmp.subscribe_type = "auto";
      tmp.flag = true;
-     tmp.cycle_unit = "YEAR";
-     body.response =  JSON.stringify(tmp);  
+     tmp.cycle_unit = "MONTH";
+     tmp.product_id = "beauty_china_subscription_1month";
+     tmp.subscribe_cycle = 1;
+     tmp.space_cap = 0;
+     body.response =  JSON.stringify(tmp);
 }
 
 $done({body: JSON.stringify(body)});
