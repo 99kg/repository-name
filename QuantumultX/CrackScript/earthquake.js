782e8b177ea1c95fa7751451847466aa32699d53
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2024-09-24 13:51:35 +0800

    Create earthquake.js

---
+++ QuantumultX/CrackScript/earthquake.js
@@ -0,0 +1,12 @@
+var obj = JSON.parse($response.body);
+obj= {
+	"code": 0,
+	"message": "查询成功",
+	"data": {
+		"endTime": 4102415999000,
+		"productId": "earthquake_early_warning_10",
+		"productName": "",
+		"upgradePrice": 0
+	}
+};
+$done({body: JSON.stringify(obj)});
