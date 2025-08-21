5f6754bab227b50822b31051925e55d800a008dc
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2025-08-21 17:26:11 +0800

    Create cf2.cookie.js

---
+++ QuantumultX/DailyBonus/02_general/cf/cf2.cookie.js
@@ -0,0 +1,59 @@
+const cookieName2 = 'cf2'
+const signurlKey2 = 'senku2_signurl_cf2'
+const signheaderKey2 = 'senku2_signheader_cf2'
+const signbodyKey2 = 'senku2_signbody_cf2'
+const senku2 = init()
+
+const requrl = $request.url
+if ($request && $request.method != 'OPTIONS') {
+  const signurlVal = requrl
+  const signheaderVal = JSON.stringify($request.headers)
+  if (signurlVal) senku2.setdata(signurlVal, signurlKey2)
+  if (signheaderVal) senku2.setdata(signheaderVal, signheaderKey2)
+    senku2.msg(cookieName2, `获取Cookie: 成功`, ``)
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+senku2.done()
