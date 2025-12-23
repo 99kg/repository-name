9b46393004535959f5cf0fc425133da332438cb3
象牙山王大拿 <64359931+99kg@users.noreply.github.com>
2025-12-23 18:46:57 +0800

    Update and rename retouch.js to rrtouch.js

--- QuantumultX/CrackScript/rrtouch.js
+++ QuantumultX/CrackScript/rrtouch.js
@@ -14,7 +14,7 @@ if(typeof $response == "undefined") {
 }else {
   let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
   if(body && body.subscriber) {
-    const product_id = "tr5_yearlysubsc_20dlrs_1";
+    const product_id = "tr5_yearlysubsc_40dlrs_cancel_discounted";
     const entitlement = "premium";
     let data = {"expires_date": "2999-01-01T00:00:00Z","original_purchase_date":"2021-01-01T00:00:00Z","purchase_date": "2021-01-01T00:00:00Z","ownership_type": "PURCHASED","store": "app_store"};
     let subscriber = body.subscriber;
